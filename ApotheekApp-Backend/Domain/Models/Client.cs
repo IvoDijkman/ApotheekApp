9b82504ecf3a620e61ed6c3b044fedb9fda227ed
Ivo Dijkman <ivo.dijkman@itvitaelearning.nl>
2024-01-16 14:25:48 +0100

    Add project files.

---
+++ ApotheekApp-Backend/Domain/Models/Client.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Objects
+{
+    public class Client
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public DateOnly DateOfBirth { get; set; }
+
+
+
+        public List<string> Allergies { get; set; }
+        public List<Medicine> Medicines { get; set; }
+    }
+}
---
+++ ApotheekApp-Backend/Domain/Models/Medicine.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Objects
+{
+    public class Medicine
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Manual {  get; set; }
+        public string Type { get; set; }
+        public List<string> Warnings { get; set; }
+
+        public int Stock {  get; set; }
+    }
+}
