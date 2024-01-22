7e71bb7487374fac236315e6448e245b51295eb7
Tamara de Weerd <animegirl_Tayko@hotmail.com>
2024-01-22 15:12:26 +0100

    Client Service and Interface startup

--- ApotheekApp-Backend/Business/Services/ClientServices.cs
+++ ApotheekApp-Backend/Business/Services/ClientServices.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ApotheekApp.Domain.Interfaces;
+using Business.Objects;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,41 @@ using System.Threading.Tasks;
 
 namespace Business.Functions
 {
-    internal class ClientServices
+    public class ClientServices : IClientService
     {
+        public Task<Client> CreateClientAsync(Client client)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteClientAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Client> GetAllClients()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Client GetClientByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Client GetClientByNameAsync(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Client> SearchClients(string query)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Client> UpdateClientAsync(Client client)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
