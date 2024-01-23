6c9a43320b1850528c59b6396287cf5ea4483a75
Thijmen Aarts <thijmen.aarts@itvitaelearning.nl>
2024-01-23 13:36:15 +0100

    Dev merged into branch and conflicts solved

--- ApotheekApp-Backend/Business/Services/EmployeeServices.cs
+++ ApotheekApp-Backend/Business/Services/EmployeeServices.cs
@@ -1,6 +1,49 @@
-﻿namespace Business.Functions
+﻿using ApotheekApp.Domain.Interfaces;
+using Business.Objects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Functions
 {
-    public class EmployeeServices
+    public class EmployeeServices : IEmployeeService
     {
+        public Task<Employee> CreateEmployeeAsync(Employee employee)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteEmployeeAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Employee> GetAllEmployees()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Employee GetEmployeeByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Employee GetEmployeeByNameAsync(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Employee> SearchEmployees(string query)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Employee> UpdateEmployeeAsync(Employee employee)
+        {
+            throw new NotImplementedException();
+        }
+
     }
 }
