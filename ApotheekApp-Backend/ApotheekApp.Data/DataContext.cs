3ade242437f264a00e780ebe13d1d3e424d9ebd9
Tamara de Weerd <animegirl_Tayko@hotmail.com>
2024-01-23 14:47:11 +0100

    Codemaid did some cleaning

--- ApotheekApp-Backend/ApotheekApp.Data/DataContext.cs
+++ ApotheekApp-Backend/ApotheekApp.Data/DataContext.cs
@@ -1,25 +1,21 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using ApotheekApp.Domain.Models;
+﻿using ApotheekApp.Domain.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using System.Diagnostics;
-using Microsoft.AspNetCore.Identity;
 
 namespace ApotheekApp.Data
 {
     public class DataContext : IdentityDbContext<AppUser>
     {
-        public DbSet<Client> Clients {  get; set; }
+        public DbSet<Client> Clients { get; set; }
         public DbSet<Employee> Employees { get; set; }
         public DbSet<Medicine> Medicines { get; set; }
 
         public DataContext() : base()
         {
-
-        } 
+        }
 
         public DataContext(DbContextOptions<DataContext> options) : base(options)
         {
-            
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
