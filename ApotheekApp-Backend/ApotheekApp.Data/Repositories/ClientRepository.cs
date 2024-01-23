aab7164aae08fbd6c0d975f061c61338fce8d361
Tamara de Weerd <animegirl_Tayko@hotmail.com>
2024-01-23 14:44:13 +0100

    set to public instead of internal

--- ApotheekApp-Backend/ApotheekApp.Data/Repositories/ClientRepository.cs
+++ ApotheekApp-Backend/ApotheekApp.Data/Repositories/ClientRepository.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ApotheekApp.Data.Repositories
+﻿namespace ApotheekApp.Data.Repositories
 {
-    internal class ClientRepository
+    public class ClientRepository
     {
     }
 }
