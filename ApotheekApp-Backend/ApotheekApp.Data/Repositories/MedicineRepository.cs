2fc80221bb6b2b5f534e9933ddea9ab2b7e76ff5
Tamara de Weerd <animegirl_Tayko@hotmail.com>
2024-02-27 14:18:19 +0100

    fixed? repos

--- ApotheekApp-Backend/ApotheekApp.Data/Repositories/MedicineRepository.cs
+++ ApotheekApp-Backend/ApotheekApp.Data/Repositories/MedicineRepository.cs
@@ -4,27 +4,22 @@ using Microsoft.EntityFrameworkCore;
 
 namespace ApotheekApp.Data.Repositories
 {
-    public class MedicineRepository : IMedicineRepository
+    public class MedicineRepository(DataContext context) : IMedicineRepository
     {
-        private readonly DataContext _context;
-        public MedicineRepository(DataContext context)
-        {
-            _context = context;
-        }
         public async Task Delete(int id)
         {
-            Medicine? medicine = await _context.Medicines.FirstOrDefaultAsync(m => m.Id == id);
+            Medicine? medicine = await context.Medicines.FirstOrDefaultAsync(m => m.Id == id);
 
             if (medicine != null)
             {
-                _context.Medicines.Remove(medicine);
-                await _context.SaveChangesAsync();
+                context.Medicines.Remove(medicine);
+                await context.SaveChangesAsync();
             }
         }
 
         public async Task<IEnumerable<Medicine>?> GetAllAsync()
         {
-            return await _context.Medicines.ToListAsync();
+            return await context.Medicines.ToListAsync();
         }
 
         public async Task<IEnumerable<Medicine>?> GetAllByClientAsync(Client client)
@@ -34,7 +29,7 @@ namespace ApotheekApp.Data.Repositories
 
         public async Task<Medicine?> GetByIdAsync(int id)
         {
-            return await _context.Medicines.FirstOrDefaultAsync(m => m.Id == id);
+            return await context.Medicines.FirstOrDefaultAsync(m => m.Id == id);
         }
     }
 }
