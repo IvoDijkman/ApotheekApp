29b0358b9a9eaf562c5c1da723fe58f415194863
Tamara de Weerd <animegirl_Tayko@hotmail.com>
2024-02-27 14:18:09 +0100

    injecting datacontext

--- ApotheekApp-Backend/ApotheekApp.Api/Program.cs
+++ ApotheekApp-Backend/ApotheekApp.Api/Program.cs
@@ -1,4 +1,5 @@
 using ApotheekApp.Business.Services;
+using ApotheekApp.Data;
 using ApotheekApp.Data.Repositories;
 using ApotheekApp.Domain.Interfaces;
 
@@ -16,12 +17,13 @@ namespace ApotheekApp.Api
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
-            builder.Services.AddScoped<ClientRepository>();
-            builder.Services.AddScoped<EmployeeRepository>();
-            builder.Services.AddScoped<MedicineRepository>();
             builder.Services.AddScoped<IClientService, ClientService>();
+            builder.Services.AddScoped<ClientRepository>();
             builder.Services.AddScoped<IEmployeeService, EmployeeService>();
+            builder.Services.AddScoped<EmployeeRepository>();
             builder.Services.AddScoped<IMedicineService, MedicineService>();
+            builder.Services.AddScoped<IMedicineRepository, MedicineRepository>();
+            builder.Services.AddDbContext<DataContext>();
 
             var app = builder.Build();
 
