952a5c97ed2a220407d17698700ab881d07179f5
Tamara de Weerd <animegirl_Tayko@hotmail.com>
2024-02-27 12:31:45 +0100

    Added clientId

--- ApotheekApp-Backend/Domain/Models/Medicine.cs
+++ ApotheekApp-Backend/Domain/Models/Medicine.cs
@@ -3,6 +3,7 @@
     public class Medicine
     {
         public int Id { get; set; }
+        public string ClientId { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public string Manual { get; set; }
