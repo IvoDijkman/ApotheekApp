aac7546f038708f94d48d00a9174a0404dda2c4d
Tamara de Weerd <animegirl_Tayko@hotmail.com>
2024-03-26 14:31:03 +0100

    added IClientRepository

---
+++ ApotheekApp-Backend/Domain/Interfaces/IClientRepository.cs
@@ -0,0 +1,21 @@
+﻿using ApotheekApp.Domain.Models;
+
+namespace ApotheekApp.Domain.Interfaces
+{
+    public interface IClientRepository
+    {
+        public Task<Client> CreateClientAsync(Client client);
+
+        public void DeleteClientAsync(string id);
+
+        public IEnumerable<Client> GetAllClients();
+
+        public Client GetClientByIdAsync(string id);
+
+        public Client GetClientByNameAsync(string lastname, DateTime dob, string? firstname);
+
+        public Client UpdateClientAsync(Client client);
+
+        public Task SaveChanges();
+    }
+}
