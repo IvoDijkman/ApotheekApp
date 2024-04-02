19be87213c40d2881d4716ca43be53a9a2686846
Tamara de Weerd <animegirl_Tayko@hotmail.com>
2024-04-02 13:43:18 +0200

    Summaries have been added

--- ApotheekApp-Backend/Domain/Interfaces/IClientRepository.cs
+++ ApotheekApp-Backend/Domain/Interfaces/IClientRepository.cs
@@ -4,18 +4,52 @@ namespace ApotheekApp.Domain.Interfaces
 {
     public interface IClientRepository
     {
+        /// <summary>
+        /// Creates a new Client.
+        /// </summary>
+        /// <param name="client"></param>
+        /// <returns></returns>
         public Task<Client> CreateClientAsync(Client client);
 
+        /// <summary>
+        /// Deletes a Client using the id.
+        /// </summary>
+        /// <param name="id"></param>
         public void DeleteClientAsync(string id);
 
+        /// <summary>
+        /// Gets a list of all Clients.
+        /// </summary>
+        /// <returns></returns>
         public IEnumerable<Client> GetAllClients();
 
+        /// <summary>
+        /// Searches for a Client by its id.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         public Client GetClientByIdAsync(string id);
 
+        /// <summary>
+        /// Search for a Client by lastname, dob, ?firstname.
+        /// </summary>
+        /// <param name="lastname"></param>
+        /// <param name="dob"></param>
+        /// <param name="firstname"></param>
+        /// <returns></returns>
         public Client GetClientByNameAsync(string lastname, DateTime dob, string? firstname);
 
+        /// <summary>
+        /// Update an existing Client.
+        /// </summary>
+        /// <param name="client"></param>
+        /// <returns></returns>
         public Client UpdateClientAsync(Client client);
 
+        /// <summary>
+        /// Save changes to db.
+        /// </summary>
+        /// <returns></returns>
         public Task SaveChanges();
     }
 }
