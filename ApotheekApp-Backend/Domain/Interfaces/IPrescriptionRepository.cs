62d21679cc95eae7db2730f3b8f27099d2dd669e
Oliver Reifler <oliver.reifler@itvitae.nl>
2024-04-04 16:57:09 +0200

    Added comments to IPrescriptionRepository Interface

--- ApotheekApp-Backend/Domain/Interfaces/IPrescriptionRepository.cs
+++ ApotheekApp-Backend/Domain/Interfaces/IPrescriptionRepository.cs
@@ -4,12 +4,30 @@ namespace ApotheekApp.Domain.Interfaces
 {
     public interface IPrescriptionRepository
     {
+        /// <summary>
+        /// Creates a Prescription async without SaveChanges.
+        /// </summary>
+        /// <param name="prescription"></param>
+        /// <returns></returns>
         public Task<Prescription> CreatePrescriptionAsync(Prescription prescription);
 
+        /// <summary>
+        /// Saves changes made to the DbSet to the database.
+        /// </summary>
+        /// <returns></returns>
         public Task SaveChangesAsync();
 
+        /// <summary>
+        /// Gets all prescriptions.
+        /// </summary>
+        /// <returns></returns>
         public Task<IEnumerable<Prescription>> GetAllAsync();
 
+        /// <summary>
+        /// Get prescription by Id.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         public Task<Prescription> GetById(int id);
     }
 }
