1929b9822843846f8c1d089b1997ac29ee713476
Thijmen Aarts <thijmen.aarts@itvitaelearning.nl>
2024-05-14 11:36:10 +0200

    Added EmployeeRepo and its interface

--- ApotheekApp-Backend/ApotheekApp.Data/Repositories/EmployeeRepository.cs
+++ ApotheekApp-Backend/ApotheekApp.Data/Repositories/EmployeeRepository.cs
@@ -1,13 +1,14 @@
-﻿using ApotheekApp.Domain.Models;
+﻿using ApotheekApp.Domain.Interfaces;
+using ApotheekApp.Domain.Models;
 
 namespace ApotheekApp.Data.Repositories
 {
-    public class EmployeeRepository(DataContext dataContext)
+    public class EmployeeRepository(DataContext dataContext) : IEmployeeRepository
     {
         public Employee GetEmployeeByIdAsync(string id) =>
         dataContext.Set<Employee>().Where(x => x.Id == id).FirstOrDefault() ?? throw new KeyNotFoundException();
 
-        public Employee GetEmployeeByNameAsync(string lastname, DateTime dob, string? firstname)
+        public Employee GetEmployeeByNameAsync(string lastname, string? firstname)
         {
             if (firstname == "")
                 return (Employee)dataContext.Set<Employee>().Where(x => x.LastName == lastname);
@@ -15,13 +16,13 @@ namespace ApotheekApp.Data.Repositories
             return (Employee)dataContext.Set<Employee>().Where(x => x.LastName == lastname && x.FirstName == firstname);
         }
 
-        public async Task<Employee> CreateEmployeeAsync(Employee employee)
+        public Employee CreateEmployee(Employee employee)
         {
-            await dataContext.AddAsync(employee);
+            dataContext.Add(employee);
             return employee;
         }
 
-        public Employee UpdateEmployeeAsync(Employee employee)
+        public Employee UpdateEmployee(Employee employee)
         {
             dataContext.Update(employee);
             return employee;
@@ -29,7 +30,7 @@ namespace ApotheekApp.Data.Repositories
 
         public IEnumerable<Employee> GetAllEmployees() => dataContext.Set<Employee>();
 
-        public void DeleteEmployeeAsync(string id)
+        public void DeleteEmployee(string id)
         {
             Employee? toDelete = dataContext.Set<Employee>().Where(x => x.Id == id).FirstOrDefault();
             if (toDelete != null)
---
+++ ApotheekApp-Backend/Domain/Interfaces/IEmployeeRepository.cs
@@ -0,0 +1,60 @@
+﻿using ApotheekApp.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApotheekApp.Domain.Interfaces
+{
+    public interface IEmployeeRepository
+    {
+        /// <summary>
+        /// Get employee by id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public Employee GetEmployeeByIdAsync(string id);
+
+        /// <summary>
+        /// Search for employee by lastname, ?firstname
+        /// </summary>
+        /// <param name="lastname"></param>
+        /// <param name="dob"></param>
+        /// <param name="firstname"></param>
+        /// <returns></returns>
+        public Employee GetEmployeeByNameAsync(string lastname, string? firstname);
+
+        /// <summary>
+        /// Create employee
+        /// </summary>
+        /// <param name="employee"></param>
+        /// <returns></returns>
+        public Employee CreateEmployee(Employee employee);
+
+        /// <summary>
+        /// Update employee
+        /// </summary>
+        /// <param name="employee"></param>
+        /// <returns></returns>
+        public Employee UpdateEmployee(Employee employee);
+
+        /// <summary>
+        /// Get a list of all employees
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<Employee> GetAllEmployees();
+
+        /// <summary>
+        /// Delete an employee
+        /// </summary>
+        /// <returns></returns>
+        public void DeleteEmployee(string id);
+
+        /// <summary>
+        /// Save changes to db.
+        /// </summary>
+        /// <returns></returns>
+        public Task SaveChanges();
+    }
+}
