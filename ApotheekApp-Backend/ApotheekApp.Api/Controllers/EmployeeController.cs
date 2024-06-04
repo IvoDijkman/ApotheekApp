0d4e0360b789cc36a4144eaa1ea4d593e4c28de5
Thijmen Aarts <thijmen.aarts@itvitaelearning.nl>
2024-06-04 13:04:56 +0200

    Employee controller added

---
+++ ApotheekApp-Backend/ApotheekApp.Api/Controllers/EmployeeController.cs
@@ -0,0 +1,102 @@
+﻿using ApotheekApp.Domain.Interfaces;
+using ApotheekApp.Domain.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApotheekApp.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : BaseController
+    {
+        private readonly IEmployeeService _employeeService;
+
+        public EmployeeController(IEmployeeService employeeService)
+        {
+            _employeeService = employeeService;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllEmployees()
+        {
+            try
+            {
+                return Ok(_employeeService.GetAllEmployees());
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetEmployeeById(string employeeId)
+        {
+            try
+            {
+                var employee = await _employeeService.GetEmployeeByIdAsync(employeeId);
+                return Ok(employee);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetEmployeeByName(string lastName, string? firstName)
+        {
+            try
+            {
+                var employee = await _employeeService.GetEmployeeByNameAsync(lastName, firstName);
+                return Ok(employee);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult CreateEmployee(Employee employee)
+        {
+            try
+            {
+                if (employee == null) { return NotFound("No employee was found"); }
+                return Ok(_employeeService.CreateEmployee(employee));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateEmployee(Employee employee)
+        {
+            try
+            {
+                var updateEmployee = await _employeeService.UpdateEmployee(employee);
+                return Ok(updateEmployee);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteEmployee(string employeeId)
+        {
+            try
+            {
+                await _employeeService.DeleteEmployee(employeeId);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
