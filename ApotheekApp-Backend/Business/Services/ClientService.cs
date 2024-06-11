4babd263368a83118205ed0674c5660bd918d591
Tamara de Weerd <animegirl_Tayko@hotmail.com>
2024-06-11 11:11:57 +0200

    fixed routes and asyncs

--- ApotheekApp-Backend/Business/Services/ClientService.cs
+++ ApotheekApp-Backend/Business/Services/ClientService.cs
@@ -29,7 +29,7 @@ namespace ApotheekApp.Business.Services
 
         public IEnumerable<Client> GetAllClients() => _clientRepository.GetAllClients();
 
-        public Client GetClientById(string id) => _clientRepository.GetClientById(id);
+        public async Task<Client> GetClientById(string id) => await _clientRepository.GetClientById(id);
 
         public async Task<Client?> GetClientByNameAsync(string lastname, DateTime dob, string? firstname) =>
             await _clientRepository.GetClientByNameAsync(lastname, dob, firstname);
