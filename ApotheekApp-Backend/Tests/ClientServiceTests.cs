64ff25afe7410cb8074764a3b8bfa70a1e2deb2c
Tamara de Weerd <animegirl_Tayko@hotmail.com>
2024-06-18 14:43:32 +0200

    progress

--- ApotheekApp-Backend/Tests/ClientServiceTests.cs
+++ ApotheekApp-Backend/Tests/ClientServiceTests.cs
@@ -17,31 +17,62 @@ namespace ApotheekApp.Tests
         private Client _Client;
         private Client _Client2;
 
-        //[Fact]
-        //private Task<Client?> GetClientByIdAsync(string id);
-        //[Fact]
-        //private Task<Client?> GetClientByNameAsync(string lastname, DateTime dob, string? firstname);
-        //[Fact]
-        //private Task<Client> CreateClientAsync(Client client);
-        //[Fact]
-        //private Task<Client> UpdateClientAsync(Client client);
-        //[Fact]
-        //private IEnumerable<Client> GetAllClients();
-        //[Fact]
-        //private Task DeleteClientAsync(string id);
+        [Fact]
+        private async Task Should_get_client_by_id()
+        {
+            Client? client = await _service.GetClientByIdAsync("yadayada");
+            Assert.Equal("TestFirstName2", client?.FirstName);
+        }
+
+        [Fact]
+        private async Task Should_find_a_client_by_name()
+        {
+            DateTime date = new(2000, 2, 2);
+            Client? client = await _service.GetClientByNameAsync("TestLastName2", date, "TestFirstName2");
+
+            Assert.Equal("TestFirstName2", client?.FirstName);
+        }
+
+        [Fact]
+        private async Task Should_create_a_client()
+        {
+            Client newclient = new() { FirstName = "newtest", LastName = "newlasttest", DateOfBirth = new DateTime(1988, 7, 6), Id = "threeeeee" };
+            await _service.CreateClientAsync(newclient);
+            Client? compare = await _service.GetClientByIdAsync("threeeeee");
+            Assert.Equal(compare?.Id, newclient.Id);
+        }
+
+        [Fact]
+        private async Task Should_update_an_existing_client()
+        {
+        }
+
+        [Fact]
+        private void Should_get_all_Clients()
+        {
+            Assert.Equal(2, _service.GetAllClients().Count()); //this gives an incorrect count
+        }
+
+        [Fact]
+        private async Task DeleteClientAsync()
+        {
+        }
+
         protected override Task SetupDatabase(DataContext context)
         {
             _Client = new Client
             {
                 FirstName = "TestFirstName1",
                 LastName = "TestLastName1",
-                Id = "Blablabla"
+                Id = "Blablabla",
+                DateOfBirth = new DateTime(1990, 1, 1)
             };
             _Client2 = new Client
             {
                 FirstName = "TestFirstName2",
                 LastName = "TestLastName2",
-                Id = "yadayada"
+                Id = "yadayada",
+                DateOfBirth = new DateTime(2000, 2, 2)
             };
             context.Add(_Client);
             context.Add(_Client2);
