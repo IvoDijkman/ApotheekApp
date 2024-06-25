5b5629748308c1cde93fb897c0230b3f58649b23
Tamara de Weerd <animegirl_Tayko@hotmail.com>
2024-06-25 12:31:26 +0200

    happy path tests done

--- ApotheekApp-Backend/Tests/ClientServiceTests.cs
+++ ApotheekApp-Backend/Tests/ClientServiceTests.cs
@@ -1,6 +1,7 @@
 ﻿using ApotheekApp.Data;
 using ApotheekApp.Domain.Interfaces;
 using ApotheekApp.Domain.Models;
+using Microsoft.EntityFrameworkCore.Migrations.Operations;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
@@ -16,6 +17,15 @@ namespace ApotheekApp.Tests
     {
         private Client _Client;
         private Client _Client2;
+        private Client _Client3;
+        private Client _Client4;
+
+        [Fact]
+        private void Should_get_all_clients()
+        {
+            Assert.Equal(14, _service.GetAllClients().Count());
+            //14 results because the seeding data is present in the test db due to inheritance.
+        }
 
         [Fact]
         private async Task Should_get_client_by_id()
@@ -36,26 +46,26 @@ namespace ApotheekApp.Tests
         [Fact]
         private async Task Should_create_a_client()
         {
-            Client newclient = new() { FirstName = "newtest", LastName = "newlasttest", DateOfBirth = new DateTime(1988, 7, 6), Id = "threeeeee" };
-            await _service.CreateClientAsync(newclient);
+            Client newClient = new() { FirstName = "newtest", LastName = "newlasttest", DateOfBirth = new DateTime(1988, 7, 6), Id = "threeeeee" };
+            await _service.CreateClientAsync(newClient);
             Client? compare = await _service.GetClientByIdAsync("threeeeee");
-            Assert.Equal(compare?.Id, newclient.Id);
+            Assert.Equal(compare?.Id, newClient.Id);
         }
 
         [Fact]
         private async Task Should_update_an_existing_client()
         {
+            _Client3.FirstName = "updatedFirst";
+            await _service.UpdateClientAsync(_Client3);
+            Client? clientCheck = await _service.GetClientByIdAsync("kekeke");
+            Assert.Equal(_Client3?.FirstName, clientCheck?.FirstName);
         }
 
         [Fact]
-        private void Should_get_all_Clients()
+        private async Task Should_delete_the_client()
         {
-            Assert.Equal(2, _service.GetAllClients().Count()); //this gives an incorrect count
-        }
-
-        [Fact]
-        private async Task DeleteClientAsync()
-        {
+            await _service.DeleteClientAsync("getnuked");
+            Assert.Null(await _service.GetClientByIdAsync("getnuked"));
         }
 
         protected override Task SetupDatabase(DataContext context)
@@ -74,8 +84,24 @@ namespace ApotheekApp.Tests
                 Id = "yadayada",
                 DateOfBirth = new DateTime(2000, 2, 2)
             };
+            _Client3 = new Client
+            {
+                FirstName = "TestFirstName3",
+                LastName = "TestLastName3",
+                Id = "kekeke",
+                DateOfBirth = new DateTime(2006, 6, 6)
+            };
+            _Client4 = new Client
+            {
+                FirstName = "TestFirstName4",
+                LastName = "TestLastName4",
+                Id = "getnuked",
+                DateOfBirth = new DateTime(1999, 8, 7)
+            };
             context.Add(_Client);
             context.Add(_Client2);
+            context.Add(_Client3);
+            context.Add(_Client4);
             return Task.CompletedTask;
         }
     }
